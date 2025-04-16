0eb85cb9e4a8797b1603a7da5344f0e74e71f297
Kyle <kyle.boudville@gmail.com>
2025-04-16 17:47:42 +0800

    deleted the abstract and children classes now moving onto working on the Dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,46 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-
-  // var car = Car(4);
-  // car.carType();
-  // car.wheelsNum();
-  // var plane = Plane(3);
-  // plane.wheelsNum();
 }
-
-//becomes a bluePrint of the child classes
-// abstract class Vehicle {
-//   Vehicle(this.wheels);
-//   int wheels;
-//   void wheelsNum();
-// }
-
-// class Car extends Vehicle {
-//   Car(super.wheels);
-//   void carType() {
-//     print("land runner");
-//   }
-
-//   @override
-//   void wheelsNum() {
-//     print("The car's wheels number is $wheels");
-//   }
-// }
-
-//Creating another class, child classes can be customizable for the blueprint
-//Child class has a contructor, because the abstract class has a constructor that takes something
-// class Plane extends Vehicle {
-//   Plane(super.wheels);
-//   void planeType() {
-//     print("land runner");
-//   }
-
-//   @override
-//   void wheelsNum() {
-//     print("The plane's wheels number is $wheels");
-//   }
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
